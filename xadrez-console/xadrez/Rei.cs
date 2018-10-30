0307eaade5a301e38508de3d9bc56cd110d178e6
Marcos Paulo Costa <mpsc_rj@hotmail.com>
2018-10-30 15:39:48 -0300

    Começando a colocar as peças no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    // Classe Torre herdando de peça
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        //sobrescrevendo o método ToString()
+        public override string ToString()
+        {
+            return "R";
+        }
+
+    }
+}
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,21 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    // Classe Torre herdando de peça
+    class Torre : Peca
+    {
+
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        //sobrescrevendo o método ToString()
+        public override string ToString()
+        {
+            return "T";
+        }
+
+    }
+}
